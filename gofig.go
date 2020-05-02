c65c1a868c9821b89c2de63326af9ec50c4ec909
Chris Reeves <hello@chris.reeves.io>
2020-05-02 14:18:45 +0100

    refactor: Parser interface w/ implementation documentation

--- gofig.go
+++ gofig.go
@@ -7,7 +7,23 @@ const (
 
 // A Parser parses configuration.
 type Parser interface {
-	Parse() error
+	// Values iterates over the configuration returning key value pairs
+	// where the key is a absolute flattened key path and valye is the keys value. The Parser
+	// should return an io.EOF error when parsing has completed, any other error value will cause
+	// parsing to error.
+	//
+	// Given parsing the following yaml:
+	//
+	//   foo:
+	//     bar:
+	//       baz: fizz
+	//
+	// The values returned by the parser would be:
+	//
+	// * key would be foo.bar.baz.
+	// * value would be fizz as a string.
+	// * err would be an io.EOF as only one key/value pair should be returned by the Parser.
+	Values() (key string, value interface{}, err error)
 }
 
 // A Notifier notifies via a channel if changes to configuration have occurred.
