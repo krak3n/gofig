7fa1132c2354ec1dc0e5e1a92a8d4b6cd2542cdd
Chris Reeves <hello@chris.reeves.io>
2020-05-02 14:23:33 +0100

    chore: update Must doc string

--- must.go
+++ must.go
@@ -1,7 +1,6 @@
 package gofig
 
-// Must calls the given function. On any error a panic will be thrown.
-// fn must be a function and must return one value which must be an error.
+// Must panics if the given error is not nil.
 func Must(err error) {
 	if err != nil {
 		panic(err)
