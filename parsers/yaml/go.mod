044adf2443cc5a21f33e2dbf12d187edc372ffad
Chris Reeves <hello@chris.reeves.io>
2020-05-09 13:10:26 +0100

    feat: base yaml parser

--- parsers/yaml/go.mod
+++ parsers/yaml/go.mod
@@ -1,3 +1,5 @@
 module go.krak3n.codes/gofig/parsers/yaml
 
 go 1.13
+
+require gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c
--- parsers/yaml/yaml.go
+++ parsers/yaml/yaml.go
@@ -1,1 +1,56 @@
 package yaml
+
+import (
+	"io"
+	"reflect"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+)
+
+// Parser parses YAML documents.
+type Parser struct {
+}
+
+// New constructs a new Parser.
+func New() *Parser {
+	return &Parser{}
+}
+
+// Values parses yaml configuration, iterating over each key value pair and returning them until
+// parsing has been completed.
+func (p *Parser) Values(src io.Reader) (<-chan func() (string, interface{}), error) {
+	var dst map[string]interface{}
+
+	d := yaml.NewDecoder(src)
+	if err := d.Decode(&dst); err != nil {
+		return nil, err
+	}
+
+	ch := make(chan func() (string, interface{}))
+
+	go func() {
+		defer close(ch)
+		recurse("", dst, ch)
+	}()
+
+	return ch, nil
+}
+
+func recurse(key string, m map[string]interface{}, ch chan func() (string, interface{})) {
+	for k, v := range m {
+		name := strings.Trim(strings.Join(append(strings.Split(key, "."), k), "."), ".")
+
+		if reflect.ValueOf(v).Kind() == reflect.Map {
+			recurse(name, v.(map[string]interface{}), ch)
+
+			continue
+		}
+
+		ch <- (func(key string, val interface{}) func() (string, interface{}) {
+			return func() (string, interface{}) {
+				return key, val
+			}
+		}(name, v))
+	}
+}
